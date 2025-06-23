c14b973325105a1a3872fd5990c6df9273e80afc
youngchae <julie1392@naver.com>
2025-06-23 11:12:53 +0900

    마녀공장

---
+++ src/main/java/kr/manyofactory/manyoshop/ManyoshopApplication.java
@@ -0,0 +1,13 @@
+package kr.manyofactory.manyoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyoshopApplication.class, args);
+	}
+
+}
